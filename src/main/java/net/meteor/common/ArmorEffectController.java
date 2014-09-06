de7c1be10acea4029fb6d8a0a87a906a5467a33d
AlexDGr8r <alextrisner@gmail.com>
2014-09-06 14:48:01 -0500

    Fix my Derpy Reflection

--- src/main/java/net/meteor/common/ArmorEffectController.java
+++ src/main/java/net/meteor/common/ArmorEffectController.java
@@ -4,36 +4,26 @@ import java.lang.reflect.Field;
 
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
-import cpw.mods.fml.common.FMLCommonHandler;
-import cpw.mods.fml.relauncher.Side;
 
 public class ArmorEffectController
 {
+	
+	private static Field fireImmunityField;
+	
+	// TODO Always check for correct field name in each update
 	public static void setImmuneToFire(EntityPlayer player, boolean flag)
 	{
 		if (player.isImmuneToFire() != flag) {
 			try {
-				Side s = FMLCommonHandler.instance().getEffectiveSide();
-				setFieldBool(Entity.class, player, flag);
-			} catch (IllegalArgumentException e) {
-				e.printStackTrace();
-			} catch (IllegalAccessException e) {
-				e.printStackTrace();
+				if (fireImmunityField == null) {
+					fireImmunityField = Entity.class.getDeclaredField("field_70178_ae"); // note that in order to test this in deobfuscated environment, change to "isImmuneToFire"
+					fireImmunityField.setAccessible(true);
+				}
+				fireImmunityField.setBoolean(player, flag);
+			} catch (Exception e) {
+				throw new RuntimeException(e);
 			}
 		}
 	}
 
-	// TODO Always check for correct field name in each update
-	private static void setFieldBool(Class class1, Object instance, boolean value) throws IllegalArgumentException, IllegalAccessException
-	{
-		try {
-			Field field = class1.getDeclaredField("field_70178_ae"); // note that in order to test this in deobfuscated environment, change to "isImmuneToFire"
-			field.setAccessible(true);
-			field.setBoolean(instance, value);
-		} catch (NoSuchFieldException e) {
-			e.printStackTrace();
-		} catch (SecurityException e) {
-			e.printStackTrace();
-		}
-	}
 }
